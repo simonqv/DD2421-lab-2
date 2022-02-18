8199f8bb05766e5b05c4b2315897d910139e15c9
erikp8 <Oxel40@users.noreply.github.com>
2022-02-18 14:10:40 +0100

    add imports and linear kernel

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello! :D")
+import random, math
+import numpy as np
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+
+
+def linear_kernel(x, y):
+    return np.dot(x, y)
